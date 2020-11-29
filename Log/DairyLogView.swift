555dfe10c333d8bb06a239d8acde76dbdcb2af01
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-11-29 15:51:28 +0900

    Add DiaryLogView

---
+++ Log/DairyLogView.swift
@@ -0,0 +1,20 @@
+//
+//  DairyLogView.swift
+//  Log
+//
+//  Created by AzumaSato on 2020/11/29.
+//
+
+import SwiftUI
+
+struct DairyLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DairyLogView_Previews: PreviewProvider {
+    static var previews: some View {
+        DairyLogView()
+    }
+}
