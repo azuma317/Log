3e2a4205c4d9f4d3a746806cb8d6c5c6f3f1725c
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-11-29 14:38:04 +0900

    Initial Commit

---
+++ Log/LogApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogApp.swift
+//  Log
+//
+//  Created by AzumaSato on 2020/11/29.
+//
+
+import SwiftUI
+
+@main
+struct LogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
