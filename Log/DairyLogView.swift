c1fe0ab1ea81cb72358f22e948127e289a84d0d9
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-11-30 23:47:27 +0900

    Update DairyLogView

--- Log/DairyLogView.swift
+++ Log/DairyLogView.swift
@@ -6,15 +6,34 @@
 //
 
 import SwiftUI
+import DynamicColor
 
 struct DairyLogView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    ZStack {
+      Color(DynamicColor(hexString: "2D2D2D"))
+        .edgesIgnoringSafeArea(.all)
+
+      VStack {
+        VStack {
+          HStack {
+            Spacer()
+
+            Text("Dairy Log")
+              .foregroundColor(
+                Color(DynamicColor(hexString: "E84F88"))
+              )
+          }
+
+          Spacer()
+        }
+      }
     }
+  }
 }
 
 struct DairyLogView_Previews: PreviewProvider {
-    static var previews: some View {
-        DairyLogView()
-    }
+  static var previews: some View {
+    DairyLogView()
+  }
 }
