24c254893b6dff8e66930d8820a6d41e6de3530e
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-12-03 00:37:01 +0900

    Add SummaryView

---
+++ Log/Supporting Views/SummaryView.swift
@@ -0,0 +1,20 @@
+//
+//  SummaryView.swift
+//  Log
+//
+//  Created by AzumaSato on 2020/11/30.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView()
+    }
+}
