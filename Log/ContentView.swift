bfe069cfb94ffe8e339ba9e6b9380c2e8286286d
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-12-06 16:38:45 +0900

    Update NavigationView in ContentView

--- Log/ContentView.swift
+++ Log/ContentView.swift
@@ -10,45 +10,9 @@ import DynamicColor
 
 struct ContentView: View {
   var body: some View {
-    ZStack {
-      Color(DynamicColor(hexString: "2D2D2D"))
-        .edgesIgnoringSafeArea(.all)
-
-      VStack {
-        Text("Index")
-          .font(.system(size: 48.0, weight: .bold))
-          .foregroundColor(.white)
-
-          .padding(.bottom)
-
-        Button(action: {}, label: {
-          Text("Dairy Log")
-            .font(.system(size: 20.0, weight: .regular))
-            .foregroundColor(.white)
-        })
-        .padding(.all, 50)
-
-        Button(action: {}, label: {
-          Text("Monthly Log")
-            .font(.system(size: 20.0, weight: .regular))
-            .foregroundColor(.white)
-        })
-        .padding(.all, 50)
-
-        Button(action: {}, label: {
-          Text("Future Log")
-            .font(.system(size: 20.0, weight: .regular))
-            .foregroundColor(.white)
-        })
-        .padding(.all, 50)
-
-        Button(action: {}, label: {
-          Text("Settings")
-            .font(.system(size: 20.0, weight: .regular))
-            .foregroundColor(.white)
-        })
-        .padding(.all, 50)
-      }
+    NavigationView() {
+      IndexView()
+        .navigationBarHidden(true)
     }
   }
 }
