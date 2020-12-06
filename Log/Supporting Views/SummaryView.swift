08b787aa1ac351b1ffe5d9e47ebec847f889d233
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-12-06 16:40:32 +0900

    Update SummaryView

--- Log/Supporting Views/SummaryView.swift
+++ Log/Supporting Views/SummaryView.swift
@@ -6,15 +6,81 @@
 //
 
 import SwiftUI
+import DynamicColor
 
 struct SummaryView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    ZStack {
+      Color(DynamicColor(hexString: "2D2D2D"))
+        .edgesIgnoringSafeArea(.all)
+
+      HStack {
+        Spacer()
+
+        Button(action: {}, label: {
+          ZStack {
+            Color(
+              DynamicColor(hexString: "2D2D2D")
+                .darkened(amount: 0.1)
+            )
+
+            VStack {
+              Spacer()
+              Text("13").font(.title)
+              Text("All").padding(.vertical, 8)
+            }
+          }
+        })
+        .frame(width: 80, height: 100, alignment: .center)
+        .foregroundColor(.white)
+
+        Spacer()
+
+        Button(action: {}, label: {
+          ZStack {
+            Color(
+              DynamicColor(hexString: "2D2D2D")
+                .lighter(amount: 0.1)
+            )
+
+            VStack {
+              Spacer()
+              Text("13").font(.title)
+              Text("All").padding(.vertical, 8)
+            }
+          }
+        })
+        .frame(width: 80, height: 100, alignment: .center)
+        .foregroundColor(.white)
+
+        Spacer()
+
+        Button(action: {}, label: {
+          ZStack {
+            Color(
+              DynamicColor(hexString: "2D2D2D")
+                .lighter(amount: 0.1)
+            )
+
+            VStack {
+              Spacer()
+              Text("13").font(.title)
+              Text("All").padding(.vertical, 8)
+            }
+          }
+        })
+        .frame(width: 80, height: 100, alignment: .center)
+        .foregroundColor(.white)
+
+        Spacer()
+      }
     }
+    .frame(width: nil, height: 120, alignment: .center)
+  }
 }
 
 struct SummaryView_Previews: PreviewProvider {
-    static var previews: some View {
-        SummaryView()
-    }
+  static var previews: some View {
+    SummaryView()
+  }
 }
