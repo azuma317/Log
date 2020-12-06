dfa2c4336f89bff440cf5085e92be9ecf88ccf8f
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-12-06 16:40:13 +0900

    Add NavigationLink in IndexView

--- Log/IndexView.swift
+++ Log/IndexView.swift
@@ -6,11 +6,50 @@
 //
 
 import SwiftUI
+import DynamicColor
 
 struct IndexView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  var body: some View {
+    ZStack {
+      Color(DynamicColor(hexString: "2D2D2D"))
+        .edgesIgnoringSafeArea(.all)
+
+      VStack {
+        Text("Index")
+          .font(.system(size: 48.0, weight: .bold))
+          .foregroundColor(.white)
+          .padding(.bottom)
+
+        NavigationLink(destination: DairyLogView().navigationBarHidden(true)) {
+          Text("Dairy Log")
+            .font(.system(size: 20.0))
+            .foregroundColor(.white)
+            .padding(.all, 50)
+        }
+
+        NavigationLink(destination: DairyLogView().navigationBarHidden(true)) {
+          Text("Monthly Log")
+            .font(.system(size: 20.0))
+            .foregroundColor(.white)
+            .padding(.all, 50)
+        }
+
+        NavigationLink(destination: DairyLogView().navigationBarHidden(true)) {
+          Text("Feture Log")
+            .font(.system(size: 20.0))
+            .foregroundColor(.white)
+            .padding(.all, 50)
+        }
+
+        NavigationLink(destination: DairyLogView().navigationBarHidden(true)) {
+          Text("Settings")
+            .font(.system(size: 20.0))
+            .foregroundColor(.white)
+            .padding(.all, 50)
+        }
+      }
     }
+  }
 }
 
 struct IndexView_Previews: PreviewProvider {
