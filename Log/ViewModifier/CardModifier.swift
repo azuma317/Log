ecd4e94e678ce7a5406cd2b037fe516fbfff883e
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-12-07 16:26:44 +0900

    Add HeaderView and CardModifier

---
+++ Log/ViewModifier/CardModifier.swift
@@ -0,0 +1,20 @@
+//
+//  CardModifier.swift
+//  Log
+//
+//  Created by AzumaSato on 2020/12/07.
+//
+
+import SwiftUI
+
+struct CardModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CardModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        CardModifier()
+    }
+}
