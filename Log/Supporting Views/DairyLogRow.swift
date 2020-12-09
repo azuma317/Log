ed618e57127c80453977f9413a4f1ceaefaba5fe
AzumaSato <lifeghtcloerce5291@gmail.com>
2020-12-09 23:53:08 +0900

    Add DairyLogRow

---
+++ Log/Supporting Views/DairyLogRow.swift
@@ -0,0 +1,20 @@
+//
+//  DairyLogRow.swift
+//  Log
+//
+//  Created by AzumaSato on 2020/12/07.
+//
+
+import SwiftUI
+
+struct DairyLogRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DairyLogRow_Previews: PreviewProvider {
+    static var previews: some View {
+        DairyLogRow()
+    }
+}
