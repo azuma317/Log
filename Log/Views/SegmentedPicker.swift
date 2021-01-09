966d6484838ac230417c11bcaa3f3ce04f770369
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-01-09 17:33:47 +0900

    Add SegmentedPicker

---
+++ Log/Views/SegmentedPicker.swift
@@ -0,0 +1,20 @@
+//
+//  SegmentedPicker.swift
+//  Log
+//
+//  Created by AzumaSato on 2021/01/07.
+//
+
+import SwiftUI
+
+struct SegmentedPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SegmentedPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        SegmentedPicker()
+    }
+}
