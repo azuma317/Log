09514224767fead239bfa55d697d3fe7c3fc8074
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-01-18 21:04:48 +0900

    Add LogListView

---
+++ Log/Views/LogListView.swift
@@ -0,0 +1,20 @@
+//
+//  LogListView.swift
+//  Log
+//
+//  Created by AzumaSato on 2021/01/18.
+//
+
+import SwiftUI
+
+struct LogListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LogListView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogListView()
+    }
+}
