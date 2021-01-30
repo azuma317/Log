f36bb082d31af72542aedd2b18fc6c14de88b6d1
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-01-30 16:12:05 +0900

    Add SetDateCell of AddTaskView

--- Log/Views/SetDateCell.swift
+++ Log/Views/SetDateCell.swift
@@ -6,15 +6,79 @@
 //
 
 import SwiftUI
+import SwiftDate
 
 struct SetDateCell: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State var isAllDay = true
+  @State var date = Date()
+  @State var showDateScreen = false
+  @State var showTimeScreen = false
+
+  var body: some View {
+    VStack {
+      HStack(alignment: .top) {
+        Image(systemName: "clock")
+          .frame(width: 16.0, height: 16.0)
+          .padding(8.0)
+
+        VStack {
+          Toggle(isOn: self.$isAllDay) {
+            Text("All Day")
+          }
+          .toggleStyle(SwitchToggleStyle())
+
+          HStack {
+            Button(action: {
+              withAnimation {
+                self.showDateScreen.toggle()
+              }
+            }, label: {
+              Text(date.in(region: .current).toString(.date(.medium)))
+            })
+
+            Spacer()
+
+            if !isAllDay {
+              Button(action: {
+                withAnimation {
+                  self.showTimeScreen.toggle()
+                }
+              }, label: {
+                Text(date.in(region: .current).toString(.time(.short)))
+              })
+            }
+          }
+        }
+        
+      }
+
+      if showDateScreen {
+        DatePicker(
+          "Start Date",
+          selection: $date,
+          displayedComponents: [.date]
+        )
+        .datePickerStyle(GraphicalDatePickerStyle())
+        .transition(.opacity)
+      }
+
+      if showTimeScreen {
+        DatePicker(
+          "",
+          selection: $date,
+          displayedComponents: [.hourAndMinute]
+        )
+        .datePickerStyle(WheelDatePickerStyle())
+        .transition(.opacity)
+      }
     }
+  }
 }
 
 struct SetDateCell_Previews: PreviewProvider {
-    static var previews: some View {
-        SetDateCell()
-    }
+  static var previews: some View {
+    SetDateCell()
+      .padding()
+      .previewLayout(PreviewLayout.sizeThatFits)
+  }
 }
