eaff8343e5168d03069e0d858a0d9c2ec3ab0a68
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-02-11 16:48:08 +0900

    Add AddMemoView

---
+++ Log/Views/AddMemoView.swift
@@ -0,0 +1,20 @@
+//
+//  AddMemoView.swift
+//  Log
+//
+//  Created by AzumaSato on 2021/02/11.
+//
+
+import SwiftUI
+
+struct AddMemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddMemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddMemoView()
+    }
+}
