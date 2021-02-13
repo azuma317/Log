8c912b507a20569ea5e5e2af818c06d41e89e866
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-02-13 13:30:30 +0900

    Add SelectLogView

---
+++ Log/Views/SelectLogView.swift
@@ -0,0 +1,20 @@
+//
+//  SelectLogView.swift
+//  Log
+//
+//  Created by AzumaSato on 2021/02/13.
+//
+
+import SwiftUI
+
+struct SelectLogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SelectLogView_Previews: PreviewProvider {
+    static var previews: some View {
+        SelectLogView()
+    }
+}
