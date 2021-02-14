f3f4d01bbc560d3e713e6dbd7533b7f5dd965932
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-02-14 17:34:30 +0900

    Add init of MonthlyLogListViewModel

--- Log/ViewModels/MonthlyLogListViewModel.swift
+++ Log/ViewModels/MonthlyLogListViewModel.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Combine
+import Resolver
+
+class MonthlyLogListViewModel: ObservableObject {
+  @Published var dayLogRepository: DayLogRepository = Resolver.resolve()
+  @Published var dayLogCellViewModels = [DayLogCellViewModel]()
+
+  private var cancellables = Set<AnyCancellable>()
+
+  init() {
+    dayLogRepository.$dayLogs.map { dayLogs in
+      dayLogs.map { dayLog in
+        DayLogCellViewModel(dayLog: dayLog)
+      }
+    }
+    .assign(to: \.dayLogCellViewModels, on: self)
+    .store(in: &cancellables)
+  }
+
+  func addDayLog(dayLog: DayLog) {
+    dayLogRepository.addDayLog(dayLog)
+  }
+
+  func removeDayLog(dayLog: DayLog) {
+    dayLogRepository.removeDayLog(dayLog)
+  }
+
+  func removeDayLogs(atOffsets indexSet: IndexSet) {
+    // remove from repo
+    let viewModels = indexSet.lazy.map { self.dayLogCellViewModels[$0] }
+    viewModels.forEach { dayLogCellViewModel in
+      removeDayLog(dayLog: dayLogCellViewModel.dayLog)
+    }
+  }
+}
