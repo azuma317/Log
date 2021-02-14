3dc30176281e28f18d4919cbacb2466be1488753
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-02-14 17:35:12 +0900

    Update layout of AddMemoView

--- Log/Views/AddMemoView.swift
+++ Log/Views/AddMemoView.swift
@@ -24,8 +24,31 @@ struct AddMemoView: View {
           dayLogCellVM.dayLogRepository.updateDayLog(dayLogCellVM.dayLog)
         }
       }
+
+      TextField("Title", text: $dayLogCellVM.dayLog.log)
+        .font(.title)
+        .padding()
+        .padding(.leading, 32.0)
+        .matchedGeometryEffect(id: "title", in: animation)
+
+      ScrollView {
+        Divider()
+
+        SetDetailCell(text: $dayLogCellVM.dayLog.description)
+
+        Divider()
+      }
+      .frame(height: loadContent ? nil : 0)
+      .opacity(loadContent ? 1 : 0)
+
+      Spacer(minLength: 0)
     }
     .background(Color(.systemBackground))
+    .onAppear {
+      withAnimation(Animation.spring().delay(0.45)) {
+        loadContent.toggle()
+      }
+    }
   }
 }
 
@@ -34,7 +57,7 @@ struct AddMemoView_Previews: PreviewProvider {
   static var previews: some View {
     AddMemoView(
       animation: animation,
-      dayLogCellVM: DayLogCellViewModel(dayLog: testDayLogs[1]),
+      dayLogCellVM: DayLogCellViewModel(dayLog: testDayLogs[0]),
       presentAddMemo: .constant(true)
     )
     .previewLayout(PreviewLayout.sizeThatFits)
