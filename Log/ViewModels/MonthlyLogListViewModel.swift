8ae7afe6faba6367e80ab1f94c6431136a8fb1da
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-02-14 17:27:52 +0900

    Add MonthlyLogView

---
+++ Log/ViewModels/MonthlyLogListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MonthlyLogListViewModel.swift
+//  Log
+//
+//  Created by AzumaSato on 2021/02/13.
+//
+
+import Foundation
---
+++ Log/Views/MonthlyLogListView.swift
@@ -0,0 +1,20 @@
+//
+//  MonthlyLogListView.swift
+//  Log
+//
+//  Created by AzumaSato on 2021/02/13.
+//
+
+import SwiftUI
+
+struct MonthlyLogListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MonthlyLogListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MonthlyLogListView()
+    }
+}
