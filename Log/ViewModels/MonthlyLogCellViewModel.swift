1a2e1ef9b42adfcd227f9d87f7976a892280c1f0
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-02-16 00:20:03 +0900

    Add MonthlyLogCell and ViewModel

---
+++ Log/ViewModels/MonthlyLogCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MonthlyLogCellViewModel.swift
+//  Log
+//
+//  Created by AzumaSato on 2021/02/15.
+//
+
+import Foundation
---
+++ Log/Views/Components/MonthlyLogCell.swift
@@ -0,0 +1,20 @@
+//
+//  MonthlyLogCell.swift
+//  Log
+//
+//  Created by AzumaSato on 2021/02/16.
+//
+
+import SwiftUI
+
+struct MonthlyLogCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MonthlyLogCell_Previews: PreviewProvider {
+    static var previews: some View {
+        MonthlyLogCell()
+    }
+}
