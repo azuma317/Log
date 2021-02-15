244628046de833d4f639c21046d16716bc5934fb
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-02-16 00:22:53 +0900

    Setup MonthlyLogListViewModel and MonthlyLogCellViewModel

--- Log/ViewModels/MonthlyLogCellViewModel.swift
+++ Log/ViewModels/MonthlyLogCellViewModel.swift
@@ -5,4 +5,25 @@
 //  Created by AzumaSato on 2021/02/15.
 //
 
-import Foundation
+import Combine
+import Resolver
+
+class MonthlyLogCellViewModel: ObservableObject {
+  @Injected var dayLogRepository: DayLogRepository
+
+  @Published var dayLogs: (key: String, value: [DayLog])
+
+  private var cancellables = Set<AnyCancellable>()
+
+  init(dayLogs: (key: String, value: [DayLog])) {
+    self.dayLogs = dayLogs
+  }
+
+  func addDayLog(dayLog: DayLog) {
+    dayLogRepository.addDayLog(dayLog)
+  }
+
+  func removeDayLog(dayLog: DayLog) {
+    dayLogRepository.removeDayLog(dayLog)
+  }
+}
--- Log/ViewModels/MonthlyLogListViewModel.swift
+++ Log/ViewModels/MonthlyLogListViewModel.swift
@@ -8,21 +8,26 @@
 import Foundation
 import Combine
 import Resolver
+import SwiftDate
 
 class MonthlyLogListViewModel: ObservableObject {
   @Published var dayLogRepository: DayLogRepository = Resolver.resolve()
-  @Published var dayLogCellViewModels = [DayLogCellViewModel]()
+  @Published var monthlyLogCellViewModels = [MonthlyLogCellViewModel]()
 
   private var cancellables = Set<AnyCancellable>()
 
   init() {
-    dayLogRepository.$dayLogs.map { dayLogs in
-      dayLogs.map { dayLog in
-        DayLogCellViewModel(dayLog: dayLog)
+    dayLogRepository.$dayLogs
+      .map { dayLogs in
+        Dictionary(grouping: dayLogs) { (dayLog) -> String in
+          dayLog.logDate.dateValue().in(region: .current).toFormat("yyyy-MM-dd")
+        }
+        .map {
+          MonthlyLogCellViewModel(dayLogs: $0)
+        }
       }
-    }
-    .assign(to: \.dayLogCellViewModels, on: self)
-    .store(in: &cancellables)
+      .assign(to: \.monthlyLogCellViewModels, on: self)
+      .store(in: &cancellables)
   }
 
   func addDayLog(dayLog: DayLog) {
