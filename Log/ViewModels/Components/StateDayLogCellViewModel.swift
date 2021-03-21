d8f37d463ce1df254edc490804acb8252875dd02
AzumaSato <lifeghtcloerce5291@gmail.com>
2021-03-21 17:37:43 +0900

    Add StateDayLogCell

---
+++ Log/ViewModels/Components/StateDayLogCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  StateDayLogCellViewModel.swift
+//  Log
+//
+//  Created by AzumaSato on 2021/03/20.
+//
+
+import Foundation
---
+++ Log/Views/Components/StateDayLogCell.swift
@@ -0,0 +1,20 @@
+//
+//  StateDayLogCell.swift
+//  Log
+//
+//  Created by AzumaSato on 2021/03/20.
+//
+
+import SwiftUI
+
+struct StateDayLogCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StateDayLogCell_Previews: PreviewProvider {
+    static var previews: some View {
+        StateDayLogCell()
+    }
+}
